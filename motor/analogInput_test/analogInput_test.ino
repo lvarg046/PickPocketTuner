2e80cdafc9559b77120ea2d98b1968d6e319059e
Luis Vargas <lvarg046@knights.ucf.edu>
2022-02-03 13:05:00 -0500

    adding tings

--- motor/analogInput_test/analogInput_test.ino
+++ motor/analogInput_test/analogInput_test.ino
@@ -5,29 +5,29 @@
  * Analog input pin: A0, used to connect the Piezosensor
  */
 
-#include <Stepper.h> // Stepper motor library
+//#include <Stepper.h> // Stepper motor library
 
 const int stepsPerRevolution = 1028;  // change this to fit the number of steps per revolution for motor
-Stepper myStepper(stepsPerRevolution, 5, 4, 14, 12); // GPIO Pins in code, corresponds to pins D1 D2 D5 D6 on board
-int stepCount = 0;  // number of steps the motor has taken
+//Stepper myStepper(stepsPerRevolution, 5, 4, 14, 12); // GPIO Pins in code, corresponds to pins D1 D2 D5 D6 on board
+//int stepCount = 0;  // number of steps the motor has taken
 void setup() {
   // Baud rate for plotter
-  Serial.begin(9600); // Change to match baud rate of motor
+  Serial.begin(115200); // Change to match baud rate of motor
 }
 
 void loop() {
   // read the sensor value from piezo:
   int sensorReading = analogRead(A0); // Reads from pin A0
   // map it to a range from 0 to 100:
-  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+//  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
   // set the motor speed:
 //  while(sensorReading< 100)
   Serial.println(sensorReading); // Offsetting to start at 0
-  if(sensorReading > 10){
-     if (motorSpeed > 0) {
-      myStepper.setSpeed(motorSpeed);
-      // step 1/100 of a revolution:
-      myStepper.step(stepsPerRevolution / 2);
-      }
-    }
+// if(sensorReading > 10){
+//     if (motorSpeed > 0) {
+//      myStepper.setSpeed(motorSpeed);
+//      // step 1/100 of a revolution:
+//      myStepper.step(stepsPerRevolution / 2);
+//      }
+//    }
 }
--- motor/motor_test/motor_test.ino
+++ motor/motor_test/motor_test.ino
@@ -4,14 +4,14 @@
 
 int i = 0;
 int motorSpeed = 250;
-unsigned long myTime;
+unsigned long myTime=0;
 bool flag = HIGH;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(15, OUTPUT); // Direction control PIN GPIO15/D8 with direction wire
   pinMode(12, OUTPUT); // PWM PIN GPIO12/D6 with PWM Wire
-  pinMode(14, OUTPUT); // FG PIN GPIO14/D5
+//  pinMode(14, OUTPUT); // FG PIN GPIO14/D5
 }
 
 void loop() {
@@ -19,29 +19,32 @@ void loop() {
 //  digitalWrite(15, HIGH);
 //  analogWrite(12, 300);
 //  Serial.print("HIGH\n");
-//  delay(10000);
+//  delay(5000);
 //  
 //  digitalWrite(15, LOW);
-//  analogWrite(12, motorSpeed);
-//  analogWrite(12, 0);
 //  Serial.print("LOW\n");
-//  delay(10000);
+Serial.println("start");
   if( millis() - myTime > 5000){
     flag = !flag;
-    Serial.print(flag);
+//    Serial.print(flag);
     digitalWrite(15, flag);
     myTime = millis();
   }
-  while( Serial.available() == 0){
-  }
-  int motorSpeed = Serial.parseInt(); // This doesn't work currently, must manually set speed to a val from 0 - 256
-  analogWrite(12, motorSpeed ); // input speed must be int
-  delay(200);
+
+//  if(Serial.available() ){
+//    analogWrite(12, Serial.parseInt());
+//    delay(200);
+//  }
+//  while( Serial.available() == 0){
+//  }
+////  int motorSpeed = Serial.parseInt(); // This doesn't work currently, must manually set speed to a val from 0 - 256
+//  analogWrite(12, motorSpeed ); // input speed must be int
+//  delay(5000);
   for( int j = 0; j<8; j++){
     i += pulseIn( 14, HIGH, 500000); // SIGNAL OUTPUT PIN GPIO14/D5 with FG wire
     // Cycle = 2*i,1s = 1000000us，Signal cycle pulse number：27*2
   }
-
+//
   i = i >> 3;
   Serial.print(111111 / i);
   Serial.println(" r/min");
